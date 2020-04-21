c6d4c6c50bc8231c3d59ac192066282e57aa1a2a
Alevtina <fedinaalevtina@gmail.com>
2020-04-21 18:39:02 +0300

    added rules for images

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+
 
 module.exports = {
     entry: { main: './src/script.js' },
@@ -20,19 +23,58 @@ module.exports = {
           },
       {
             test: /\.css$/,
-            use:  [MiniCssExtractPlugin.loader, 'css-loader']
-           }
+            use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+           },
+           {
+            test: /\.(eot|ttf|woff|woff2)$/,
+            loader: 'file-loader?name=./vendor/[name].[ext]'
+        },
+        {
+            test: /\.css$/i,
+            use: [
+                            (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                            'css-loader', 
+                            'postcss-loader'
+                    ]
+    },
+    {
+        test: /\.(gif|png|jpe?g|ico|svg)$/i,
+        use: [
+            'file-loader?name=./images/[name].[ext]', 
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                  bypassOnDebug: true,
+                  disable: true,
+              },
+            },
+          ],
+    }
         ]
       },
       plugins: [ 
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css'
         }),
+        new webpack.DefinePlugin({
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
         new HtmlWebpackPlugin({
           inject: false,
           template: './src/index.html',
           filename: 'index.html'
         }),
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'
+    }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                    preset: ['default'],
+            },
+            canPrint: true
+    }), // подключите плагин после MiniCssExtractPlugin
         new WebpackMd5Hash()
       ]
     };
